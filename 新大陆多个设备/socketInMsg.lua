7cf3983956bcc8992f48499efb750d58677fbf17
chain01 <15863338830@163.com>
2018-11-11 15:36:28 +0800

    多个设备，根据一个设备修改

---
+++ 新大陆多个设备/socketInMsg.lua
@@ -0,0 +1,57 @@
+--- 模块功能：socket客户端数据接收处理
+-- @author openLuat
+-- @module socketLongConnectionTrasparent.socketInMsg
+-- @license MIT
+-- @copyright openLuat
+-- @release 2018.03.28
+
+module(...,package.seeall)
+require"pins"
+--- socket客户端数据接收处理
+-- @param socketClient，socket客户端对象
+-- @return 处理成功返回true，处理出错返回false
+-- @usage socketInMsg.proc(socketClient)
+function proc(socketClient)
+    local result,data
+	local Heart_Str="$OK##\r"
+	local Rec_Data
+	local Rec_Status
+	local Rec_Cmd
+	local key = pins.setup(pio.P0_6,1)
+    while true do
+        result,data = socketClient:recv(2000)
+        --接收到数据
+        if result then
+            log.info("socketInMsg.proc",data)
+			if string.find(data,"$#AT") then
+				 sys.publish("UART_RECV_DATA",Heart_Str)
+            end   
+			if string.find(data,"status") then
+				Rec_Data=json.decode(data)
+				Rec_Status=Rec_Data["status"]
+				--log.info("Rec_Status",Rec_Status)  --控制台打印status值供调试
+				if Rec_Status==0 then
+					sys.publish("SOCKET_RECV_DATA","OK")
+				end
+            end
+			if string.find(data,"data") then
+				Rec_Data=json.decode(data)
+				Rec_Cmd=Rec_Data["data"]
+				if	Rec_Cmd==1 then
+					sys.publish("SOCKET_RECV_DATA","On")
+					key(0)
+					sys.wait(1000)          -- 挂起1000ms，同理为每隔1000ms运行一次
+					key(1)
+					else
+					sys.publish("SOCKET_RECV_DATA","Off")
+				end
+			end	
+            --如果socketOutMsg中有等待发送的数据，则立即退出本循环
+            if socketOutMsg.waitForSend() then return true end
+        else
+            break
+        end
+    end
+	
+    return result or data=="timeout"
+end
---
+++ 新大陆多个设备/socketOutMsg.lua
@@ -0,0 +1,66 @@
+--- 模块功能：socket客户端数据发送处理
+-- @author openLuat
+-- @module socketLongConnectionTrasparent.socketOutMsg
+-- @license MIT
+-- @copyright openLuat
+-- @release 2018.03.28
+
+
+module(...,package.seeall)
+
+--数据发送的消息队列
+local msgQueue = {}
+
+local function insertMsg(data,user)
+    table.insert(msgQueue,{data=data,user=user})
+end
+
+--发送连接请求
+--@string device 设备标识 
+--@string key 传输密钥
+function link()
+	local device="testkey01"
+	local key="92b7ae622ea74262b9f0db8302272965"
+	local Json_Key
+	local Json_Str1=[[{"t": 1,"device": "]]
+	local Json_Str2=[[","key":"]]
+	local Json_Str3=[[","ver":"v1.1"}]]
+	Json_Key=Json_Str1..device..Json_Str2..key..Json_Str3
+	insertMsg(Json_Key)
+end
+
+--- 去初始化“socket客户端数据发送”
+-- @return 无
+-- @usage socketOutMsg.unInit()
+function unInit()
+    while #msgQueue>0 do
+        local outMsg = table.remove(msgQueue,1)
+        if outMsg.user and outMsg.user.cb then outMsg.user.cb(false,outMsg.user.para) end
+    end
+end
+
+--- socket客户端是否有数据等待发送
+-- @return 有数据等待发送返回true，否则返回false
+-- @usage socketOutMsg.waitForSend()
+function waitForSend()
+    return #msgQueue > 0
+end
+
+--- socket客户端数据发送处理
+-- @param socketClient，socket客户端对象
+-- @return 处理成功返回true，处理出错返回false
+-- @usage socketOutMsg.proc(socketClient)
+function proc(socketClient)
+    while #msgQueue>0 do
+        local outMsg = table.remove(msgQueue,1)
+        local result = socketClient:send(outMsg.data)
+        if outMsg.user and outMsg.user.cb then outMsg.user.cb(result,outMsg.user.para) end
+        if not result then return end
+    end
+    return true
+end
+
+local function uartRecvData(data)
+    insertMsg(data)
+end
+sys.subscribe("UART_RECV_DATA",uartRecvData)
